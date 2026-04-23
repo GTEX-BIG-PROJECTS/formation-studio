ddde7518871ee3470a6aa25266d4a86d666b23a7
Mohamed Cisse <mohammedciss11@gmail.com>
2026-04-23 09:02:43 -0400

    add Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import FormationStudio from './FormationStudio';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FormationStudio />
+    <Analytics />
   </React.StrictMode>
 );
